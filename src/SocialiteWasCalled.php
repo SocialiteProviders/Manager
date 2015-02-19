37a2637b02e1f7a325224e4f9b8d7cb343771833
Andy Wendt <andy@awendt.com>
2015-02-18 18:18:59 -0700

    Added ability to extend socialite through event object

--- src/SocialiteWasCalled.php
+++ src/SocialiteWasCalled.php
@@ -3,4 +3,19 @@ namespace AndyWendt\Socialite\Extender;
 
 class SocialiteWasCalled
 {
+    /**
+     * @param string $providerName 'meetup'
+     * @param string $providerClass 'Your\Name\Space\ClassName'
+     */
+    public function extendSocialite($providerName, $providerClass)
+    {
+        $socialite = \App::make('Laravel\Socialite\Contracts\Factory');
+        $socialite->extend(
+            $providerName,
+            function ($app) use ($socialite, $providerName, $providerClass) {
+                $config = $app['config']['services.' . $providerName];
+                return $socialite->buildProvider($providerClass, $config);
+            }
+        );
+    }
 }
