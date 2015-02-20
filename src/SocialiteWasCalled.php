15af236cd0bf3eabaed10eab81768ec8e2712c2c
DraperStudio <hello@draperstud.io>
2015-02-20 21:14:15 +0100

    Add OAuth1 Provider support

--- src/SocialiteWasCalled.php
+++ src/SocialiteWasCalled.php
@@ -4,16 +4,30 @@ namespace SocialiteProviders\Manager;
 class SocialiteWasCalled
 {
     /**
-     * @param string $providerName 'meetup'
-     * @param string $providerClass 'Your\Name\Space\ClassName'
+     * @param string $providerName   'meetup'
+     * @param string $providerClass  'Your\Name\Space\ClassNameProvider'
+     * @param string $providerServer 'Your\Name\Space\ClassNameServer'
      */
-    public function extendSocialite($providerName, $providerClass)
+    public function extendSocialite($providerName, $providerClass, $providerServer = null)
     {
         $socialite = app()->make('Laravel\Socialite\Contracts\Factory');
+
         $socialite->extend(
             $providerName,
-            function ($app) use ($socialite, $providerName, $providerClass) {
-                $config = $app['config']['services.' . $providerName];
+            function ($app) use ($socialite, $providerName, $providerClass, $providerServer) {
+                $config = $app['config']['services.'.$providerName];
+
+                if (!empty($providerServer)) {
+                    return new $providerClass(
+                        $app['request'],
+                        new $providerServer([
+                            'identifier'   => $config['client_id'],
+                            'secret'       => $config['client_secret'],
+                            'callback_uri' => $config['redirect'],
+                        ])
+                    );
+                }
+
                 return $socialite->buildProvider($providerClass, $config);
             }
         );
