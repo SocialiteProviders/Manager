9c88a0a6078926b7553339cc218c1b63c7837028
DraperStudio <hello@draperstudio.tech>
2016-03-29 07:30:56 +0200

    Add missing use statement

--- src/Contracts/OAuth1/ProviderInterface.php
+++ src/Contracts/OAuth1/ProviderInterface.php
@@ -2,9 +2,10 @@
 
 namespace SocialiteProviders\Manager\Contracts\OAuth1;
 
+use Laravel\Socialite\One\ProviderInterface as SocialiteOauth1ProviderInterface;
 use SocialiteProviders\Manager\Contracts\ConfigInterface as Config;
 
-interface ProviderInterface extends SocialiteOauth2ProviderInterface
+interface ProviderInterface extends SocialiteOauth1ProviderInterface
 {
     /**
      * @param Config $config
