44c4ab7dae517301d18fe15c716b48198f6195ca
Andy Wendt <andy@awendt.com>
2016-03-29 11:44:58 -0600

    fixed service provider test

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -4,9 +4,12 @@ namespace SocialiteProviders\Manager\Test;
 
 use Mockery as m;
 use SocialiteProviders\Manager\ServiceProvider;
+use SocialiteProviders\Manager\SocialiteWasCalled;
 
 class ServiceProviderTest extends \PHPUnit_Framework_TestCase
 {
+    use ManagerTestTrait;
+
     public function tearDown()
     {
         \Mockery::close();
@@ -17,13 +20,27 @@ class ServiceProviderTest extends \PHPUnit_Framework_TestCase
      */
     public function it_fires_an_event()
     {
-        $app = m::mock(\Illuminate\Contracts\Foundation\Application::class);
-        $socialiteWasCalled = m::mock(\SocialiteProviders\Manager\SocialiteWasCalled::class);
+        $socialiteWasCalledMock = m::mock(SocialiteWasCalled::class);
+        self::$functions->shouldReceive('app')->with(SocialiteWasCalled::class)->once()->andReturn($socialiteWasCalledMock);
+        $socialiteWasCalledMock = m::mock(\SocialiteProviders\Manager\SocialiteWasCalled::class);
 
-        $event = m::mock(\Illuminate\Contracts\Events\Dispatcher::class);
-        $event->shouldReceive('fire')->with($socialiteWasCalled);
+        self::$functions->shouldReceive('event')->with($socialiteWasCalledMock)->once();
 
-        $sp = new ServiceProvider($app);
-        $sp->boot($event, $socialiteWasCalled);
+        $sp = new ServiceProvider(m::mock(\Illuminate\Container\Container::class));
+        $sp->boot();
     }
+}
+
+namespace SocialiteProviders\Manager;
+
+use SocialiteProviders\Manager\Test\ServiceProviderTest;
+
+function app($make)
+{
+    return ServiceProviderTest::$functions->app($make);
+}
+
+function event($event)
+{
+    return ServiceProviderTest::$functions->event($event);
 }
