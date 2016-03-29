a15a317273dd9d4f7c86520958ebf965f87ddef8
DraperStudio <hello@draperstudio.tech>
2016-03-29 07:37:30 +0200

    Remove invalid interface

--- src/Contracts/OAuth1/ProviderInterface.php
+++ src/Contracts/OAuth1/ProviderInterface.php
@@ -2,10 +2,9 @@
 
 namespace SocialiteProviders\Manager\Contracts\OAuth1;
 
-use Laravel\Socialite\One\ProviderInterface as SocialiteOauth1ProviderInterface;
 use SocialiteProviders\Manager\Contracts\ConfigInterface as Config;
 
-interface ProviderInterface extends SocialiteOauth1ProviderInterface
+interface ProviderInterface
 {
     /**
      * @param Config $config
