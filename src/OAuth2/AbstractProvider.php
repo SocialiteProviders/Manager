d4d820bce591b27e2c441e35d22d1ddc6214a6fd
DraperStudio <hello@draperstudio.tech>
2016-06-03 19:54:03 +0200

    Remove deprecated method `getAccessToken`

--- src/OAuth2/AbstractProvider.php
+++ src/OAuth2/AbstractProvider.php
@@ -2,6 +2,7 @@
 
 namespace SocialiteProviders\Manager\OAuth2;
 
+use Illuminate\Support\Arr;
 use GuzzleHttp\ClientInterface;
 use Laravel\Socialite\Two\InvalidStateException;
 use SocialiteProviders\Manager\Contracts\OAuth2\ProviderInterface;
@@ -32,38 +33,21 @@ abstract class AbstractProvider extends BaseProvider implements ProviderInterfac
             throw new InvalidStateException();
         }
 
+        $response = $this->getAccessTokenResponse($this->getCode());
+
         $user = $this->mapUserToObject($this->getUserByToken(
-            $token = $this->getAccessToken($this->getCode())
+            $token = $this->parseAccessToken($response)
         ));
 
-        $user->setToken($token);
+        $this->credentialsResponseBody = $response;
 
         if ($user instanceof User) {
             return $user->setAccessTokenResponseBody($this->credentialsResponseBody);
         }
 
-        return $user;
-    }
-
-    /**
-     * Get the access token for the given code.
-     *
-     * @param string $code
-     *
-     * @return string
-     */
-    public function getAccessToken($code)
-    {
-        $postKey = (version_compare(ClientInterface::VERSION, '6') === 1) ? 'form_params' : 'body';
-
-        $response = $this->getHttpClient()->post($this->getTokenUrl(), [
-            'headers' => ['Accept' => 'application/json'],
-            $postKey => $this->getTokenFields($code),
-        ]);
-
-        $this->credentialsResponseBody = json_decode($response->getBody(), true);
-
-        return $this->parseAccessToken($response->getBody());
+        return $user->setToken($token)
+                    ->setRefreshToken(Arr::get($response, 'refresh_token'))
+                    ->setExpiresIn(Arr::get($response, 'expires_in'));
     }
 
     /**
@@ -75,6 +59,6 @@ abstract class AbstractProvider extends BaseProvider implements ProviderInterfac
      */
     protected function parseAccessToken($body)
     {
-        return json_decode($body, true)['access_token'];
+        return Arr::get($body, 'access_token');
     }
 }
