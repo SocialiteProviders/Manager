c7b497462baa77197857dad4779ba940e84d6077
Alexander Sterpu <alex@finalsoft.ru>
2016-08-14 23:15:06 +0300

    Update SocialiteWasCalled.php

--- src/SocialiteWasCalled.php
+++ src/SocialiteWasCalled.php
@@ -52,6 +52,7 @@ class SocialiteWasCalled
                 if (defined('SOCIALITEPROVIDERS_STATELESS') && SOCIALITEPROVIDERS_STATELESS) {
                     return $provider->stateless();
                 }
+                
                 return $provider;
             }
         );
