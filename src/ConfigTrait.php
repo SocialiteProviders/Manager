7ad32446d71a2d04445df785e23e09fd051b5965
Brian Faust <faustbrian@users.noreply.github.com>
2018-01-04 20:12:28 +0200

    Apply fixes from StyleCI (#114)
    [ci skip] [skip ci]

--- src/ConfigTrait.php
+++ src/ConfigTrait.php
@@ -18,10 +18,10 @@ trait ConfigTrait
     {
         $config = $config->get();
 
-        $this->config       = $config;
-        $this->clientId     = $config['client_id'];
+        $this->config = $config;
+        $this->clientId = $config['client_id'];
         $this->clientSecret = $config['client_secret'];
-        $this->redirectUrl  = $config['redirect'];
+        $this->redirectUrl = $config['redirect'];
 
         return $this;
     }
@@ -44,7 +44,7 @@ trait ConfigTrait
     {
         // check manually if a key is given and if it exists in the config
         // this has to be done to check for spoofed additional config keys so that null isn't returned
-        if (!empty($key) && empty($this->config[$key])) {
+        if (! empty($key) && empty($this->config[$key])) {
             return $default;
         }
 
